2f754010ff274ca1998399451d7bc8c949b47c6c
komosyu4649 <komosyu4649@gmail.com>
2025-11-12 09:33:53 +0900

    Refactor Appendix component for improved responsiveness and layout
    - Adjusted width and gap settings for better responsiveness across different screen sizes.
    - Enhanced text wrapping and alignment to improve readability and user engagement.
    - Updated margin and whitespace properties for a more polished presentation.
    
    These changes contribute to a more user-friendly experience in the Appendix section of the Leasur page.

--- src/components/sections/leasur/Appendix.tsx
+++ src/components/sections/leasur/Appendix.tsx
@@ -3,20 +3,20 @@ import React from "react";
 const Appendix = () => {
   return (
     <section className="relative flex size-full flex-col items-center gap-10 px-0 py-20">
-      <div className="flex w-[64.375rem] flex-col items-start gap-10 md:w-80">
+      <div className="flex w-[64.375rem] flex-col items-start gap-10 md:w-full">
         {/* Text Section */}
-        <div className="flex flex-col items-start gap-2">
+        <div className="flex flex-col items-start gap-2 md:gap-2">
           <p className="whitespace-pre text-center font-helvetica text-base font-bold leading-4 text-[#2ba1db]">
             ●Appendix
           </p>
           <p className="whitespace-pre font-gothic text-[24px] leading-[1.5] tracking-[0.96px] text-[#333333]">
             利用料金
           </p>
-          <div className="relative inline-grid place-items-start whitespace-nowrap font-gothic leading-[0] text-[#333333]">
+          <div className="relative inline-grid place-items-start whitespace-nowrap font-gothic leading-[0] text-[#333333] md:whitespace-normal">
             <p className="relative whitespace-pre text-[20px] leading-[28px]">
               月額：5,000円/1ユーザー
             </p>
-            <div className="relative mt-[31px] whitespace-pre text-base leading-6">
+            <div className="relative mt-[31px] whitespace-pre text-base leading-6 md:mt-[3px] md:whitespace-normal">
               <p className="mb-0">
                 ご希望に応じて貴社専用にカスタマイズ開発を行うことも可能です。
               </p>
