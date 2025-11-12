3daddbf47c61d14e1ea9cbe64d1227e56434f7c0
komosyu4649 <komosyu4649@gmail.com>
2025-11-12 09:25:50 +0900

    Refactor FAQ component for improved responsiveness and layout consistency
    - Adjusted styling to enhance responsiveness across different screen sizes, including width and gap modifications.
    - Improved text wrapping and alignment for better readability and user engagement.
    - Ensured consistent design elements with previous updates in the Leasur section.
    
    These changes contribute to a more polished and user-friendly experience in the FAQ section.

--- src/components/sections/leasur/Faq.tsx
+++ src/components/sections/leasur/Faq.tsx
@@ -4,9 +4,9 @@ import { FAQ_DATA } from "@/const/leasur";
 const Faq = () => {
   return (
     <section className="relative flex size-full flex-col items-center gap-10 bg-slate-100 px-0 py-20">
-      <div className="flex flex-col items-start gap-10">
+      <div className="flex flex-col items-start gap-10 md:w-[348px]">
         {/* Title Section */}
-        <div className="flex flex-col items-start gap-4 leading-[0]">
+        <div className="flex flex-col items-start gap-4 leading-[0] md:gap-4">
           <div className="relative inline-grid place-items-start whitespace-pre text-nowrap">
             <p className="relative font-helvetica text-base leading-4 text-[#2ba1db]">
               ●FAQ
@@ -21,15 +21,15 @@ const Faq = () => {
             {FAQ_DATA.map((faq, index) => (
               <div
                 key={index}
-                className="flex w-[64.688rem] flex-col gap-[3px] rounded-[2px] border border-solid border-black bg-white px-8 py-4 font-gothic text-[#333333]"
+                className="flex w-[64.688rem] flex-col gap-[3px] rounded-[2px] border border-solid border-black bg-white px-8 py-4 font-gothic text-[#333333] md:w-[350px] md:gap-2"
               >
                 {/* Question */}
-                <p className="whitespace-pre text-[20px] leading-[28px]">
+                <p className="whitespace-pre text-[20px] leading-[28px] md:w-full md:whitespace-normal">
                   {faq.question}
                 </p>
 
                 {/* Answer */}
-                <div className="whitespace-pre text-base leading-6">
+                <div className="whitespace-pre text-base leading-6 md:w-full md:whitespace-normal">
                   {faq.answer.map((line, i) => (
                     <p
                       key={i}
